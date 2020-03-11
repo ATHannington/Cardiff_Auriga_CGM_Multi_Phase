74a1cbc92f9f96ad876e2bf15d6ed1901dad8e3c
ATHannington <33061879+ATHannington@users.noreply.github.com>
2020-03-11 14:54:09 +0000

    Added Tracers v1.0. Can select cells based on condition, and relevant tracers, then go from those tracers back to cells.

--- auriga/Tracers.py
+++ auriga/Tracers.py
@@ -6,6 +6,81 @@ import const as c
 from gadget import *
 from gadget_subfind import *
 from Snapper import *
+
+def GetTracersFromCells(snapGas, snapTracers,Cond):
+    print("GetTracersFromCells")
+
+    print("CellIDs initial from Cond")
+    #Select CellIDs for Cond
+    CellIDs = snapGas.id[Cond]
+    print(CellIDs)
+    print(np.shape(CellIDs))
+
+    #Select Parent IDs in Cond list
+    #   Selecting from cells with a tracer, which cell IDs are have cell IDs that meet Cond
+    print("Parents")
+    Parents = np.isin(snapTracers.prid,CellIDs)
+    print(Parents)
+    print(np.shape(Parents))
+    #Select the data for Cells that meet Cond which contain tracers
+    #   Does this by making a new dictionary from old data
+    #       but only selects values where Parent ID is True
+    #           i.e. where Cond is met and Cell ID contains tracers
+    print("Cells with tracers")
+    Cells = {key: value for ind, (key, value) in enumerate(snapGas.data.items()) if Parents[ind] == True}
+
+    #Select INDICES of the tracers who are in a cell which meets Cond
+    print("ParentsIndices")
+    ParentsIndices = np.where(Parents)
+    print(ParentsIndices)
+    print(np.shape(ParentsIndices))
+
+    print("CellIDs final Containing Tracers Only")
+    CellIDs = snapGas.id[np.where(np.isin(snapGas.id,snapTracers.prid[ParentsIndices]))]
+    print(CellIDs)
+    print(np.shape(CellIDs))
+
+
+    #Finally, select TRACER IDs who are in cells that meet Cond
+    print("Tracers")
+    Tracers = snapTracers.trid[ParentsIndices]
+    print(Tracers)
+    print(np.shape(Tracers))
+
+
+    return Tracers, Cells, CellIDs
+
+#------------------------------------------------------------------------------#
+def GetCellsFromTracers(snapGas, snapTracers,Tracers):
+    print("GetCellsFromTracers")
+
+    print(Tracers)
+    print(np.shape(Tracers))
+    print("Tracers Indices")
+    TracersIndices = np.where(np.isin(snapTracers.trid,Tracers))
+    print(TracersIndices)
+    print(np.shape(TracersIndices))
+
+    print("Parents")
+    Parents = snapTracers.prid[TracersIndices]
+    print(Parents)
+    print(np.shape(Parents))
+
+    print("CellsIndices")
+    CellsIndices = np.isin(snapGas.id,Parents)
+    print(CellsIndices)
+    print(np.shape(CellsIndices))
+    print("Cells")
+    Cells = {key: value for ind, (key, value) in enumerate(snapGas.data.items()) if CellsIndices[ind] == True}
+
+    print("CellIDs")
+    CellIDs = snapGas.id[np.where(CellsIndices)]
+    print(CellIDs)
+    print(np.shape(CellIDs))
+    return Cells, CellIDs
+
+#==============================================================================#
+
 simfile='/home/universe/spxtd1-shared/ISOTOPES/output/' # set paths to simulation
 snapnum=127 # set snapshot to look at
 
@@ -18,6 +93,7 @@ elements_solar = [12.0, 10.93, 8.43,  7.83,  8.69,  7.93,  7.60,  7.51,  7.50,  
 Zsolar = 0.0127
 
 omegabaryon0 = 0.048
+#==============================================================================#
 
 
 # Single FvdV Projection:
@@ -53,48 +129,33 @@ print("cond")
 #Select INDICES of T = 10^5+-delta K
 Cond = np.where((snapGas.data['T']>=1.*10**(target-delta)) & (snapGas.data['T']<=1.*10**(target+delta)))
 
-print("CellIDs")
-#Select CellIDs for Cond
-CellIDs = snapGas.id[Cond]
+Tracers, CellsTFC, CellIDsTFC = GetTracersFromCells(snapGas, snapTracers,Cond)
+CellsCFT, CellIDsCFT = GetCellsFromTracers(snapGas, snapTracers,Tracers)
 
-#Select the data for Cells that meet Cond
-#   Does this by making a new dictionary from old data
-#       but only selects values where index of entry (given by enumerate)
-#           is found in Cond list (with [0] to access the list as array, not tuple)
-print("Cells")
-Cells = {key: value for ind, (key, value) in enumerate(snapGas.data.items()) if ind in Cond[0]}
-
-#Select Parent IDs in Cond list
-#   Selecting from cells with a tracer, which cell IDs are have cell IDs that meet Cond
-print("Parents")
-Parents = np.isin(CellIDs,snapTracers.prid)
-
-print("Cells with tracers")
-Cells = {key: value for ind, (key, value) in enumerate(snapGas.data.items()) if Parents[ind] == True}
-#Select INDICES of the tracers who are in a cell which meets Cond
-print("ParentsIndices")
-ParentsIndices = np.where(Parents)
-
-#Finally, select TRACER IDs who are in cells that meet Cond
-print("Tracers")
-Tracers = snapTracers.trid[ParentsIndices]
+print("Analysis Complete!")
+print("Cell IDs Tracers from Cells:", CellIDsTFC)
+print("Shape:",np.shape(CellIDsTFC))
+print("Cell IDs Cells from Tracers:", CellIDsCFT)
+print("Shape:",np.shape(CellIDsCFT))
+print("")
+print("Matching set of Cell IDs Tracers from Cell selection and Cell Selection from Tracers? ==","\n", np.all(np.isin(CellIDsTFC,CellIDsCFT)))
+print("")
+print("END!")
 
 
-
-
-print("Snapnum")
-Cells['snapnum']=[snapnum for i in range(0,len(Cells['T']))]
-subset = 100
-print("Plot!")
-fig = plt.figure()
-ax = plt.gca()
-ax.scatter(Cells['snapnum'][:subset],Cells['T'][:subset])
-ax.set_yscale('log')
-ax.set_xlim(snapnum-1,snapnum+1)
-ax.set_xlabel("Snap Number")
-ax.set_ylabel("Temperature [K]")
-ax.set_title(f"Subset of first {subset:0.0f} Cell Temperatures Containing Tracers")
-opslaan = f'Tracers.png'
-plt.savefig(opslaan, dpi = 500, transparent = True)
-print(opslaan)
-plt.close()
+# print("Snapnum")
+# Cells['snapnum']=[snapnum for i in range(0,len(Cells['T']))]
+# subset = 100
+# print("Plot!")
+# fig = plt.figure()
+# ax = plt.gca()
+# ax.scatter(Cells['snapnum'][:subset],Cells['T'][:subset])
+# ax.set_yscale('log')
+# ax.set_xlim(snapnum-1,snapnum+1)
+# ax.set_xlabel("Snap Number")
+# ax.set_ylabel("Temperature [K]")
+# ax.set_title(f"Subset of first {subset:0.0f} Cell Temperatures Containing Tracers")
+# opslaan = f'Tracers.png'
+# plt.savefig(opslaan, dpi = 500, transparent = True)
+# print(opslaan)
+# plt.close()
