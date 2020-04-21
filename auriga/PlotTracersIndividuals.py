Tracers V4.5. Fixed issues with padding in wrong order. Implemented better units. Added medians of new parameters. Fixed PlotTracersPDF PlotTracersIndividuals for star (type 4) tracking.

auriga/PlotTracersIndividuals.py
@@ -15,8 +15,7 @@ from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,AutoMinorLoca
 import const as c
 from gadget import *
 from gadget_subfind import *
-from Snapper import *
-import pickle
+import h5py
 from Tracers_Subroutines import *
 from random import sample
 
@@ -28,9 +27,16 @@ opacity = 0.5#0.01
 #Input parameters path:
 TracersParamsPath = 'TracersParams.csv'
 
-saveParams = ['T', 'R', 'n_H', 'B']
+saveParams = ['T','R','n_H','B','vrad','gz','L','P_thermal','P_magnetic','P_kinetic']
 
-ylabel={'T': r'Temperature [$K$]', 'R': r'Radius [$kpc$]', 'n_H':r'$n_H$ [c$m^{-3}$]', 'B':r'|B| [$\mu G$]'}
+ylabel={'T': r'Log10 Temperature [$K$]', 'R': r'Radius [$kpc$]',\
+ 'n_H':r'Log10 $n_H$ [$cm^{-3}$]', 'B':r'Log10 |B| [$\mu G$]',\
+ 'vrad':r'Log10 Radial Velocity [$km$ $s^{-1}$]',\
+ 'gz':r'Log10 Average Metallicity', 'L':r'Log10 Specific Angular Momentum[$km^{2}$ $s^{-2}$]',\
+ 'P_thermal':r'Log10 Thermal Pressure [$erg$ $cm^{-2}]',\
+ 'P_magnetic':r'Log10 Magnetic Pressure [$\mu G$ $sr^{-1}]',\
+ 'P_kinetic': r'Log10 Kinetic Pressure [$kg$ $km^2$ $s^-2$]'\
+ }
 
 #==============================================================================#
 
@@ -65,9 +71,8 @@ for analysisParam in saveParams:
 
         key = (f"T{int(T)}",f"{int(TRACERSPARAMS['snapnum'])}")
         rangeMin = 0
-        rangeMax = len(dataDict[key][analysisParam])
+        rangeMax = len(dataDict[key]['trid'])
         TracerNumberSelect = np.arange(start=rangeMin, stop = rangeMax, step = 1 )
-
         #Take Random sample of Tracers size min(subset, len(data))
         TracerNumberSelect = sample(TracerNumberSelect.tolist(),min(subset,rangeMax))
         SelectedTracers1 = dataDict[key]['trid'][TracerNumberSelect]
@@ -79,14 +84,14 @@ for analysisParam in saveParams:
         tmpMassdata =[]
         for snap in range(int(TRACERSPARAMS['snapMin']),min(int(TRACERSPARAMS['snapMax']+1),int(TRACERSPARAMS['snapnumMAX']+1))):
             key = (f"T{int(T)}",f"{int(snap)}")
-
+            whereGas = np.where(dataDict[key]['type']==0)[0]
             #Get Individual Cell Data from selected Tracers.
             #   Not all Tracers will be present at all snapshots, so we return a NaN value in that instance.
             #   This allows for plotting of all tracers for all snaps they exist.
             #   Grab data for analysisParam and mass.
             data, massData, _ = GetIndividualCellFromTracer(Tracers=dataDict[key]['trid'],\
-                Parents=dataDict[key]['prid'],CellIDs=dataDict[key]['id'],SelectedTracers=SelectedTracers1,\
-                Data=dataDict[key][analysisParam],mass=dataDict[key]['mass'])
+                Parents=dataDict[key]['prid'],CellIDs=dataDict[key]['id'][whereGas],SelectedTracers=SelectedTracers1,\
+                Data=dataDict[key][analysisParam][whereGas],mass=dataDict[key]['mass'][whereGas])
 
             #Append the data from this snapshot to a temporary list
             tmpXdata.append(dataDict[key]['Lookback'][0])
@@ -138,7 +143,7 @@ for analysisParam in saveParams:
         #Get temperature
         temp = TRACERSPARAMS['targetTLst'][ii]
 
-        plotYdata = Ydata[f"T{int(temp)}"][:,:,0]
+        plotYdata = Ydata[f"T{int(temp)}"]
 
         #Set style options
         opacityPercentiles = 0.25
@@ -168,7 +173,7 @@ for analysisParam in saveParams:
         ax[ii].tick_params(which='both')
         if (analysisParam != 'R'):
             ax[ii].set_yscale('log')
-        ax[ii].set_ylabel(ylabel[analysisParam],fontsize=8)
+        ax[ii].set_ylabel(ylabel[analysisParam],fontsize=15)
         if (analysisParam == 'T'):
             ax[ii].set_ylim(ymin=1e3, ymax=1e7)
         fig.suptitle(f"Cells Containing Tracers selected by: " +\
@@ -176,16 +181,17 @@ for analysisParam in saveParams:
         r" and $%05.2f \leq R \leq %05.2f kpc $"%(TRACERSPARAMS['Rinner'], TRACERSPARAMS['Router']) +\
         "\n" + f" and selected at snap {TRACERSPARAMS['snapnum']:0.0f}"+\
         f" weighted by mass" +\
-        "\n" + f"Subset of {int(subset)} Individual Tracers at each Temperature Plotted" )
+        "\n" + f"Subset of {int(subset)} Individual Tracers at each Temperature Plotted" \
+        , fontsize=12)
         ax[ii].legend(loc='upper right')
 
 
     #Only give 1 x-axis a label, as they sharex
-    ax[len(Tlst)-1].set_xlabel(r"Lookback Time [$Gyrs$]",fontsize=8)
+    ax[len(Tlst)-1].set_xlabel(r"Lookback Time [$Gyrs$]",fontsize=15)
 
     plt.tight_layout()
     plt.subplots_adjust(top=0.90, wspace = 0.005)
-    opslaan = f"Tracers{int(TRACERSPARAMS['snapnum'])}"+analysisParam+str(int(subset))+f"_Individuals.pdf"
+    opslaan = f"Tracers_selectSnap{int(TRACERSPARAMS['snapnum'])}_"+analysisParam+"_"+str(int(subset))+f"_Individuals.pdf"
     plt.savefig(opslaan, dpi = DPI, transparent = False)
     print(opslaan)
     plt.close()

auriga/PlotTracersPDF.py
@@ -14,8 +14,7 @@ import matplotlib.pyplot as plt
 import const as c
 from gadget import *
 from gadget_subfind import *
-from Snapper import *
-import pickle
+import h5py
 from Tracers_Subroutines import *
 
 Nbins = 100
@@ -28,9 +27,16 @@ TracersParamsPath = 'TracersParams.csv'
 #Entered parameters to be saved from
 #   n_H, B, R, T
 #   Hydrogen number density, |B-field|, Radius [kpc], Temperature [K]
-saveParams = ['T','R','n_H','B']
-
-xlabel={'T': r'Log10 Temperature [$K$]', 'R': r'Radius [$kpc$]', 'n_H':r'Log10 $n_H$ [c$m^{-3}$]', 'B':r'Log10 |B| [$\mu G$]'}
+saveParams = ['T','R','n_H','B','vrad','gz','L','P_thermal','P_magnetic','P_kinetic']
+selectedSnaps = [112,119,127]
+xlabel={'T': r'Log10 Temperature [$K$]', 'R': r'Radius [$kpc$]',\
+ 'n_H':r'Log10 $n_H$ [$cm^{-3}$]', 'B':r'Log10 |B| [$\mu G$]',\
+ 'vrad':r'Log10 Radial Velocity [$km$ $s^{-1}$]',\
+ 'gz':r'Log10 Average Metallicity', 'L':r'Log10 Specific Angular Momentum[$km^{2}$ $s^{-2}$]',\
+ 'P_thermal':r'Log10 Thermal Pressure [$erg$ $cm^{-2}]',\
+ 'P_magnetic':r'Log10 Magnetic Pressure [$\mu G$ $sr^{-1}]',\
+ 'P_kinetic': r'Log10 Kinetic Pressure [$kg$ $km^2$ $s^-2$]'\
+ }
 
 
 TRACERSPARAMS, DataSavepath, Tlst = LoadTracersParameters(TracersParamsPath)
@@ -84,14 +90,18 @@ for dataKey in saveParams:
             plotData.update({key: sorted_data})
 
         #Loop over snaps from snapMin to snapmax, taking the snapnumMAX (the final snap) as the endpoint if snapMax is greater
-        for snap in range(int(TRACERSPARAMS['snapMin']), int(min(TRACERSPARAMS['snapnumMAX']+1, TRACERSPARAMS['snapMax']+1))):
+        for snap in selectedSnaps:#range(int(TRACERSPARAMS['snapMin']), int(min(TRACERSPARAMS['snapnumMAX']+1, TRACERSPARAMS['snapMax']+1))):
 
             fig, ax = plt.subplots(nrows=1, ncols=1, figsize = (xsize,ysize), dpi = DPI)
 
             dictkey = (f"T{int(T)}",f"{int(snap)}")
 
             whereGas = np.where(dataDict[dictkey]['type'] == 0)
-            whereStars = np.where(dataDict[dictkey]['type'] == 4)
+
+            dataDict[dictkey]['age'][np.where(np.isnan(dataDict[dictkey]['age']) == True)] = 0.
+
+            whereStars = np.where((dataDict[dictkey]['type'] == 4)&\
+                                  (dataDict[dictkey]['age'] >= 0.))
 
             NGas = len(dataDict[dictkey]['type'][whereGas])
             NStars = len(dataDict[dictkey]['type'][whereStars])
@@ -103,8 +113,8 @@ for dataKey in saveParams:
             data = dataDict[dictkey][dataKey]
             weights = dataDict[dictkey]['mass']
 
-            xmin = np.min(np.log10(data))
-            xmax = np.max(np.log10(data))
+            xmin = np.nanmin(np.log10(data))
+            xmax = np.nanmax(np.log10(data))
             #
             # step = (xmax-xmin)/Nbins
             #
@@ -131,7 +141,7 @@ for dataKey in saveParams:
             r" and $%05.2f \leq R \leq %05.2f kpc $"%(TRACERSPARAMS['Rinner'], TRACERSPARAMS['Router']) +\
             "\n" + f" and selected at snap {TRACERSPARAMS['snapnum']:0.0f}"+\
             f" weighted by mass"+\
-            "\n" + f"{percentage:0.03f}% of Tracers in Stars",fontsize=15)
+            "\n" + f"{percentage:0.03f}% of Tracers in Stars",fontsize=12)
             ax.axvline(x=vline, c='red')
 
             plt.tight_layout()

auriga/Tracers_Subroutines.py
@@ -147,6 +147,7 @@ def CalculateTrackedParameters(snapGas,elements,elements_Z,elements_mass,element
     snapGas.data['R'] =  (np.linalg.norm(snapGas.data['pos'], axis=1)) #[Kpc]
 
     #Radial Velocity [km s^-1]
+    KpcTokm = 1e3*c.parsec*1e-5
     snapGas.data['vrad'] = (snapGas.pos*snapGas.vel).sum(axis=1)
     snapGas.data['vrad'] /= snapGas.data['R']
 
@@ -160,19 +161,19 @@ def CalculateTrackedParameters(snapGas,elements,elements_Z,elements_mass,element
     #Load in Star Formation Rate
     tmp = snapGas.data['sfr']
 
-    #Specific Angular Momentum [kpc km s^-1]
-    snapGas.data['L'] = sqrt((cross(snapGas.data['pos'], snapGas.data['vel'])**2.).sum(axis=1))
+    #Specific Angular Momentum [km^2 s^-1]
+    snapGas.data['L'] = sqrt((cross(snapGas.data['pos']*KpcTokm, snapGas.data['vel'])**2.).sum(axis=1))
 
     snapGas.data['ndens'] = snapGas.data['dens']/(meanweight*c.amu)
 
     #Thermal Pressure : P = n Kb T
     snapGas.data['P_thermal'] = snapGas.ndens * c.KB *snapGas.T
 
-    #Magnetic Pressure [microGauss ^ 2]
+    #Magnetic Pressure [microGauss ^ 2 sr^-1]
     snapGas.data['P_magnetic'] = (snapGas.data['B'] **2)/( 8. * pi)
 
-    #Kinetic "Pressure" [10^10 Msol km^2 ^s^-2]
-    snapGas.data['P_kinetic'] = snapGas.rho * (np.linalg.norm(snapGas.data['vel'][whereGas], axis=1))**2
+    #Kinetic "Pressure" [Kg km^2 ^s^-2]
+    snapGas.data['P_kinetic'] = snapGas.rho *1e10 *c.msol *1e-3* (np.linalg.norm(snapGas.data['vel'][whereGas], axis=1))**2
 
     del tmp
 
@@ -256,8 +257,21 @@ def GetIndividualCellFromTracer(Tracers,Parents,CellIDs,SelectedTracers,Data,mas
         if element == True:
             parent = Parents[ind]
             dataIndex = np.where(np.isin(CellIDs,parent))
-            saveData.append(Data[dataIndex].tolist())
-            massData.append(mass[dataIndex].tolist())
+
+            dataEntry = Data[dataIndex].tolist()
+            if (np.shape(dataEntry)[0] == 0):
+                dataEntry =np.nan
+            else:
+                dataEntry = dataEntry[0]
+
+            massEntry = mass[dataIndex].tolist()
+            if (np.shape(massEntry)[0] == 0):
+                massEntry =np.nan
+            else:
+                massEntry = massEntry[0]
+
+            saveData.append(dataEntry)
+            massData.append(massEntry)
         else:
             saveData.append([np.nan])
             massData.append([np.nan])
@@ -356,7 +370,8 @@ def PadNonEntries(snapGas):
 
             elif(np.shape(value)[0] == NStars):
                 listValues = value.tolist()
-                paddedList = listValues + GasNone
+                #Opposite addition order to maintain sensible ordering.
+                paddedList = GasNone + listValues
                 if (len(paddedList) != NTot):
                     print("[@ STARS @PadNonEntries:] Padded List not of length NTot. Data Does not have non-entries for GAS!")
                 paddedValues = np.array(paddedList)