f34a96d7da483c2ba35246fb5aff48166f951f0a
ATHannington <33061879+ATHannington@users.noreply.github.com>
2020-06-26 17:13:28 +0100

    Updated Snapper to accomodate for multiprocessing.

--- auriga/Snapper.py
+++ auriga/Snapper.py
@@ -5,11 +5,14 @@ import matplotlib.pyplot as plt
 import const as c
 from gadget import *
 from gadget_subfind import *
+import multiprocessing as mp
 
 
-
-simfile='/home/universe/spxtd1-shared/ISOTOPES/output/' # set paths to simulation
+simfile="/home/universe/spxfv/Auriga/level4_MHD_variants/halo_18_3000snaps/output/"#'/home/universe/spxtd1-shared/ISOTOPES/output/' # set paths to simulation
 snapnum=127 # set snapshot to look at
+snap_start = 2500
+snap_end = 2999
+n_processes = 6
 
 class Snapper(object):
     """
@@ -163,9 +166,10 @@ class Snapper(object):
 #         Plot Projections and Slices
 #------------------------------------------------------------------------------#
 
-    def PlotProjections(self,Snap,SnapNum,SnapDM=None,SnapStars=None,Axes=[0,1],boxsize = 200.,boxlos = 50.,pixres = 1.,pixreslos = 0.1, Nbins=500, DPI = 500,CMAP='inferno'):
+    def PlotProjections(self,Snap,SnapNum,SnapDM=None,SnapStars=None,Axes=[0,1],boxsize = 400.,boxlos = 50.,pixres = 1.,pixreslos = 0.1, Nbins=500, DPI = 500,CMAP='inferno'):
 
-        print("Projection!")
+        print("Quad Plot...")
+        print("Calculating Tracked Parameters!")
 
         #Axes Labels to allow for adaptive axis selection
         AxesLabels = ['x','y','z']
@@ -176,7 +180,6 @@ class Snapper(object):
         #--------------------------#
         ##    Units Conversion    ##
         #--------------------------#
-
         #Density is rho/ <rho> where <rho> is average baryonic density
         rhocrit = 3. * (Snap.omega0 * (1+Snap.redshift)**3. + Snap.omegalambda) * (Snap.hubbleparam * 100*1e5/(c.parsec*1e6))**2. / ( 8. * pi * c.G)
         rhomean = 3. * (Snap.omega0 * (1+Snap.redshift)**3.) * (Snap.hubbleparam * 100*1e5/(c.parsec*1e6))**2. / ( 8. * pi * c.G)
@@ -198,10 +201,15 @@ class Snapper(object):
         #--------------------------#
         ## Slices and Projections ##
         #--------------------------#
+        print("Slices and Projections!")
 
-        # slice_nH    = Snap.get_Aslice("n_H", box = [boxsize,boxsize],\
-        #  center = imgcent, nx = int(boxsize/pixres), ny = int(boxsize/pixres),\
-        #  axes = Axes, proj = False, numthreads=16)
+        slice_nH    = Snap.get_Aslice("n_H", box = [boxsize,boxsize],\
+         center = imgcent, nx = int(boxsize/pixres), ny = int(boxsize/pixres),\
+         axes = Axes, proj = False, numthreads=1)
+
+        slice_B   = Snap.get_Aslice("B", box = [boxsize,boxsize],\
+         center = imgcent, nx = int(boxsize/pixres), ny = int(boxsize/pixres),\
+         axes = Axes, proj = False, numthreads=1)
 
         # slice_T     = Snap.get_Aslice("T", box = [boxsize,boxsize],\
         #  center = imgcent, nx = int(boxsize/pixres), ny = int(boxsize/pixres),\
@@ -215,13 +223,13 @@ class Snapper(object):
         #  center = imgcent, nx = int(boxsize/pixres), ny = int(boxsize/pixres),\
         #  nz = int(boxlos/pixreslos), boxz = boxlos, axes = Axes, proj = True, numthreads=16)
 
-        proj_nH     = Snap.get_Aslice("n_H", box = [boxsize,boxsize],\
-         center = imgcent, nx = int(boxsize/pixres), ny = int(boxsize/pixres),\
-         nz = int(boxlos/pixreslos), boxz = boxlos, axes = Axes, proj = True, numthreads=16)
-
-        proj_B     = Snap.get_Aslice("B", box = [boxsize,boxsize],\
-         center = imgcent, nx = int(boxsize/pixres), ny = int(boxsize/pixres),\
-         nz = int(boxlos/pixreslos), boxz = boxlos, axes = Axes, proj = True, numthreads=16)
+        # proj_nH     = Snap.get_Aslice("n_H", box = [boxsize,boxsize],\
+        #  center = imgcent, nx = int(boxsize/pixres), ny = int(boxsize/pixres),\
+        #  nz = int(boxlos/pixreslos), boxz = boxlos, axes = Axes, proj = True, numthreads=16)
+        #
+        # proj_B     = Snap.get_Aslice("B", box = [boxsize,boxsize],\
+        #  center = imgcent, nx = int(boxsize/pixres), ny = int(boxsize/pixres),\
+        #  nz = int(boxlos/pixreslos), boxz = boxlos, axes = Axes, proj = True, numthreads=16)
 
         #   DM mass stored separately as singular value and needs to be
         #   converted into array for weighting of hist2D
@@ -248,6 +256,8 @@ class Snapper(object):
         #[0] to remove from numpy array for purposes of plot title
         lookback = Snap.cosmology_get_lookback_time_from_a(np.array([aConst]))[0] #[Gyrs]
 
+        print("Plot!")
+
         #DPI Controlled by user as lower res needed for videos #
         fig, axes = plt.subplots(2, 2, figsize = (xsize,ysize), dpi = DPI)
 
@@ -260,7 +270,7 @@ class Snapper(object):
         #-----------#
         # Plot DM Histogram 2D #
         #-----------#
-
+        # print("pcm1")
         pcm1 = axes[0,0].hist2d(SnapDM.pos[:,Axes[0]],SnapDM.pos[:,Axes[1]],bins=Nbins,range=[[-1.*halfbox,halfbox],[-1.*halfbox,halfbox]],
         norm = matplotlib.colors.LogNorm(), weights=massDM , cmap = cmap)
 
@@ -270,16 +280,16 @@ class Snapper(object):
         #-----------#
         # Plot n_H Projection #
         #-----------#
-
-        pcm2 = axes[0,1].pcolormesh(proj_nH['x'], proj_nH['y'], np.transpose(proj_nH['grid'])/int(boxlos/pixreslos),\
-         norm = matplotlib.colors.LogNorm(vmin=1e-6,vmax=1e-1), cmap = cmap, rasterized = True)
-        axes[0,1].set_title(r'Projection $n_H$')
+        # print("pcm2")
+        pcm2 = axes[0,1].pcolormesh(slice_nH['x'], slice_nH['y'], np.transpose(slice_nH['grid']),\
+        norm = matplotlib.colors.LogNorm(vmin=1e-6,vmax=1e-1), cmap = cmap, rasterized = True)
+        axes[0,1].set_title(r'Slice $n_H$ centred at z=0 kpc')
         fig.colorbar(pcm2, ax = axes[0,1], orientation = 'horizontal',label=r'$n_H$ [$cm^{-3}$]')
 
         #-----------#
         # Plot Stars Histogram 2D #
         #-----------#
-
+        # print("pcm3")
         pcm3 = axes[1,0].hist2d(SnapStars.pos[:,Axes[0]],SnapStars.pos[:,Axes[1]],bins=NbinsStars,\
         range=[[-1.*halfbox,halfbox],[-1.*halfbox,halfbox]], weights=SnapStars.mass, norm = matplotlib.colors.LogNorm(), cmap = cmap)
 
@@ -289,16 +299,16 @@ class Snapper(object):
         #-----------#
         # Plot Magnetic Field Projection #
         #-----------#
-
-        pcm4 = axes[1,1].pcolormesh(proj_B['x'], proj_B['y'], np.transpose(proj_B['grid']),\
-        norm = matplotlib.colors.SymLogNorm(linthresh=0.1),\
-         cmap = cmap, rasterized = True)
-
-        axes[1,1].set_title(r'Projection $B$')
+        # print("pcm4")
+        pcm4 = axes[1,1].pcolormesh(slice_B['x'], slice_B['y'], np.transpose(slice_B['grid']),\
+        norm = matplotlib.colors.LogNorm(),\
+        cmap = cmap, rasterized = True)
+        # print("foo!")
+        axes[1,1].set_title(r'Slice $B$ centred at z=0 kpc')
         fig.colorbar(pcm4, ax = axes[1,1], orientation = 'horizontal',label=r'$B$ [$\mu G$]')
 
         #-----------#
-
+        print("AxesLabels")
         #Add Axes labels to each plot, adaptive to which axes are selected
         for ii in range(0,2):
             for jj in range(0,2):
@@ -309,20 +319,50 @@ class Snapper(object):
         axes[0,1].set_aspect(1.0)
         axes[1,0].set_aspect(1.0)
         axes[1,1].set_aspect(1.0)
-
+        print("SnapNum")
         #Pad snapnum with zeroes to enable easier video making
         SnapNum = str(SnapNum).zfill(3);
         opslaan = f'Shaded_Cell_{SnapNum}.png'
+        print(f"Save {opslaan}")
         plt.savefig(opslaan, dpi = DPI, transparent = True)
-        print(opslaan)
         plt.close()
+
+        print("...done!")
 #------------------------------------------------------------------------------#
 #           Plot (NSnaps - Start)x Projections
 #
 #               CURRENTLY ONLY DEFAULTS SUPPORTED
 #------------------------------------------------------------------------------#
+    def _projection_movie(self,snap,SimDirectory):
+        snapper = Snapper()
+        print(f"Starting {snap}")
+        # load in the subfind group files
+        snap_subfind = load_subfind(snap,dir=SimDirectory)
 
-    def ProjectionMovieLoop(self,SimDirectory,NSnaps=127,Start=10):
+        # load in the gas particles mass and position. 0 is gas, 1 is DM, 4 is stars, 5 is BHs
+        snapGas   = gadget_readsnap(snap, SimDirectory,loadonlytype = [0],lazy_load=True)
+        snapDM    = gadget_readsnap(snap, SimDirectory,loadonlytype = [1],lazy_load=True)
+        snapStars = gadget_readsnap(snap, SimDirectory,loadonlytype = [4],lazy_load=True)
+
+        print(f" SnapShot loaded at RedShift z={snapGas.redshift:0.05e}")
+
+        snapGas   = snapper.SetCentre(Snap=snapGas,Snap_SubFind=snap_subfind,HaloID=0)
+        snapDM    = snapper.SetCentre(Snap=snapDM,Snap_SubFind=snap_subfind,HaloID=0)
+        snapStars = snapper.SetCentre(Snap=snapStars,Snap_SubFind=snap_subfind,HaloID=0)
+
+        #Convert Units
+        ## Make this a seperate function at some point??
+        snapGas.pos   *= 1e3 #[kpc]
+        snapDM.pos    *= 1e3 #[kpc]
+        snapStars.pos *= 1e3 #[kpc]
+
+        snapGas.vol *= 1e9 #[kpc^3]
+
+        snapper.PlotProjections(Snap=snapGas,SnapNum=snap,SnapDM=snapDM,SnapStars=snapStars,DPI=200)
+
+        return
+
+    def ProjectionMovieLoop(self,SimDirectory,Start,End,n_processes):
         """
         Function for Histogram 2D plotting of a loop (Start to NSnaps) Snaps.
         Args:
@@ -330,52 +370,26 @@ class Snapper(object):
             NSnaps              : Opt. Number of SnapShots      : Default = 127
             Start               : Opt. Start Snapshot           : Default = 10 . Below this Halo finder may fail
         """
-        # import matplotlib.animation as manimation
 
+        # import matplotlib.animation as manimation
         self.SimDirectory = SimDirectory
 
-        print("Let's make a Movie!")
+        args_default = [self.SimDirectory]
+        args_list = [[snap]+args_default for snap in range(Start,End+1)]
+
         snapper = Snapper()
-        for ii in range(Start,NSnaps+1):
-            #Create child Snapper for each histogram. Caution, don't add to self
-            # or data will stack up and bug out.
+        print("\n" + f"Opening {n_processes} core Pool!")
+        print("Let's make a Movie!")
+        pool = mp.Pool(processes=n_processes)
 
-            #Load Snap and plot histogram with default options
-            #
-            ###
-            # Andy: generalise this later, please!
-            ###
-            #
-            #
+        #Compute movie projections!
+        [pool.apply_async(snapper._projection_movie,args=args) for args in args_list]
 
-            # load in the subfind group files
-            snap_subfind = load_subfind(ii,dir=simfile)
-
-            # load in the gas particles mass and position. 0 is gas, 1 is DM, 4 is stars, 5 is BHs
-            snapGas   = gadget_readsnap(ii, simfile,loadonlytype = [0],lazy_load=True)
-            snapDM    = gadget_readsnap(ii, simfile,loadonlytype = [1],lazy_load=True)
-            snapStars = gadget_readsnap(ii, simfile,loadonlytype = [4],lazy_load=True)
-
-            print(f" SnapShot loaded at RedShift z={snapGas.redshift:0.05e}")
-
-            snapGas   = snapper.SetCentre(Snap=snapGas,Snap_SubFind=snap_subfind,HaloID=0)
-            snapDM    = snapper.SetCentre(Snap=snapDM,Snap_SubFind=snap_subfind,HaloID=0)
-            snapStars = snapper.SetCentre(Snap=snapStars,Snap_SubFind=snap_subfind,HaloID=0)
-
-            #Convert Units
-            ## Make this a seperate function at some point??
-            snapGas.pos   *= 1e3 #[kpc]
-            snapDM.pos    *= 1e3 #[kpc]
-            snapStars.pos *= 1e3 #[kpc]
-
-            snapGas.vol *= 1e9 #[kpc^3]
-
-            snapper.PlotProjections(Snap=snapGas,SnapNum=ii,SnapDM=snapDM,SnapStars=snapStars,DPI=200)
-
-            #Print percentage complete
-            print(f"{(float(ii + 1 - Start)/float(NSnaps - Start))*100.0 : 0.03f}","% complete")
+        pool.close()
+        pool.join()
 
         return
+
 #===============================================================================#
 
 #===============================================================================#
@@ -437,7 +451,7 @@ class Snapper(object):
 # Snapper3.PlotProjections(Snap=snapGas,SnapNum=snapnum,SnapDM=snapDM,SnapStars=snapStars)
 
 #------------------------------------------------------------------------------#
-
-# # Histogram 2D Movie:
-# Snapper4 = Snapper()
-# Snapper4.ProjectionMovieLoop(SimDirectory=simfile,NSnaps=127,Start=10)
+10
+# Histogram 2D Movie:
+Snapper4 = Snapper()
+Snapper4.ProjectionMovieLoop(SimDirectory=simfile,Start=snap_start,End=snap_end,n_processes=n_processes)
