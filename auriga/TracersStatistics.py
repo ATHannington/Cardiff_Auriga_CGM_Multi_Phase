30c31c634057b0d2d16c2dc06d310d678ff459c1
ATHannington <33061879+ATHannington@users.noreply.github.com>
2020-11-02 16:54:29 +0000

    Fixed Tracers Statistics Version 1. Added Statistics plots (same format as clusters) and a much improved Stacked PDF using Seaborn.

--- auriga/TracersStatistics.py
+++ auriga/TracersStatistics.py
@@ -21,7 +21,21 @@ DataSavepathSuffix = f".h5"
 dtwJoint = True
 
 ignoreParams = ['clusters', 'd_crit', 'maxmimally_distinct_bool', 'sort_level']
+StatsEntries = ['Cluster','Snap','Lookback',"T","%Tracers",'%Halo0','%Unbound','%OtherHalo',"%NoHalo",'%Stars',"%ISM",'%Wind',"%Inflow","%Outflow"]
 
+#==============================================================================#
+#       Chemical Properties
+#==============================================================================#
+#element number   0     1      2      3      4      5      6      7      8      9      10     11     12      13
+elements       = ['H',  'He',  'C',   'N',   'O',   'Ne',  'Mg',  'Si',  'Fe',  'Y',   'Sr',  'Zr',  'Ba',   'Pb']
+elements_Z     = [1,    2,     6,     7,     8,     10,    12,    14,    26,    39,    38,    40,    56,     82]
+elements_mass  = [1.01, 4.00,  12.01, 14.01, 16.00, 20.18, 24.30, 28.08, 55.85, 88.91, 87.62, 91.22, 137.33, 207.2]
+elements_solar = [12.0, 10.93, 8.43,  7.83,  8.69,  7.93,  7.60,  7.51,  7.50,  2.21,  2.87,  2.58,  2.18,   1.75]
+
+Zsolar = 0.0127
+
+omegabaryon0 = 0.048
+#==============================================================================#
 #==============================================================================#
 
 #Load Analysis Setup Data
@@ -61,9 +75,7 @@ snapsRange = np.array([ xx for xx in range(int(TRACERSPARAMS['snapMin']), min(in
 ################################################################################
 
 
-StatsEntries = ['ClusterID','Snap','Lookback',"%Tracers",'%Halo0','%Unbound','%OtherHalo',"%NoHalo",'%Stars',"%ISM",'%Wind',"%Inflow","%Outflow","%AboveZsolar","%BelowZsolar","%Heating","%Cooling"]
-
-def _inner_analysis(dataDict,T,ClusterID,FullKey):
+def _inner_analysis(dataDict,T,ClusterID,FullKey,NtracersFull):
 
     lookback = []
     temp = []
@@ -80,16 +92,18 @@ def _inner_analysis(dataDict,T,ClusterID,FullKey):
     inflow = []
     outflow = []
 
-    NtracersFull = dataDict[FullKey]['Ntracers'][0]
-    tracers.append(100.)
-
+    NtracersAll = dataDict[FullKey]['Ntracers'][0]
+    tracers.append(100.*(NtracersAll/NtracersFull))
+    # print("")
+    # print(f"T{T} FullKey{FullKey} cluster{ClusterID}")
+    # print(f"All {NtracersAll} : FullData {NtracersFull}")
     SubHalo = dataDict[FullKey]['SubHaloID']
 
     whereGas = np.where(dataDict[FullKey]['type']==0)[0]
 
     Ntracers = np.shape(whereGas)[0]
 
-    gas.append(100.*(Ntracers/NtracersFull))
+    gas.append(100.*(Ntracers/NtracersAll))
 
     halo0.append(100.*(np.shape(np.where(SubHalo[whereGas]==int(TRACERSPARAMS['haloID']))[0])[0]/Ntracers))
 
@@ -101,11 +115,11 @@ def _inner_analysis(dataDict,T,ClusterID,FullKey):
     noHalo.append(100.*(np.shape(np.where((SubHalo[whereGas]!=int(TRACERSPARAMS['haloID']))\
         &(SubHalo[whereGas]!=-1)&(np.isnan(SubHalo[whereGas])==True))[0])[0]/Ntracers))
 
-    stars.append(100.*(np.shape(np.where((dataDict[FullKey]['age']>0)&(dataDict[FullKey]['type']==4))[0])[0]/NtracersFull))
+    stars.append(100.*(np.shape(np.where((dataDict[FullKey]['age']>=0)&(dataDict[FullKey]['type']==4))[0])[0]/NtracersAll))
 
-    wind.append(100.*(np.shape(np.where((dataDict[FullKey]['age']<0)&(dataDict[FullKey]['type']==4))[0])[0]/NtracersFull))
+    wind.append(100.*(np.shape(np.where((dataDict[FullKey]['age']<0)&(dataDict[FullKey]['type']==4))[0])[0]/NtracersAll))
 
-    ism.append(100.*(np.shape(np.where((dataDict[FullKey]['sfr']>0)&(dataDict[FullKey]['R']<=25.))[0])[0]/NtracersFull))
+    ism.append(100.*(np.shape(np.where((dataDict[FullKey]['sfr']>0)&(dataDict[FullKey]['R']<=25.))[0])[0]/NtracersAll))
 
     inflow.append(100.*(np.shape(np.where(dataDict[FullKey]['vrad'][whereGas]<0.)[0])[0]/Ntracers))
     outflow.append(100.*(np.shape(np.where(dataDict[FullKey]['vrad'][whereGas]>0.)[0])[0]/Ntracers))
@@ -135,12 +149,28 @@ def fullData_analyse_cluster(dataDict,paramStatsDict,Tlst,snapsRange,ClusterID=-
         wind = []
         inflow = []
         outflow = []
-        for snap in snapsRange:
-            FullKey = (f"T{T}",f"{int(snap)}")
+        for ii, snap in enumerate(snapsRange):
+            tmpsnapdataDict = {}
+            for k, v in dataDict[Tkey].items():
+                if(np.shape(np.shape(v))[0]>1):
+                    if(np.shape(v)[1]>1):
+                        tmpsnapdataDict.update({k : v[ii,:]})
+                    else:
+                        tmpsnapdataDict.update({k : v[ii]})
+                else:
+                    tmpsnapdataDict.update({k : v})
+            tmpsnapdataDict.update({'Ntracers' : [np.shape(tmpsnapdataDict['type'])[0]]})
+            snapdataDict = {Tkey : tmpsnapdataDict}
+            NtracersFull = dataDict[Tkey]['Ntracers'][ii]
+            # FullKey = (f"T{T}",f"{int(snap)}")
+            # tmplookback, tmptemp, tmpcluster ,tmptracers,\
+            #  tmphalo0 , tmpunbound, tmpotherHalo ,tmpnoHalo ,\
+            #   tmpgas, tmpstars, tmpism , tmpwind ,tmpinflow, tmpoutflow \
+            #   = _inner_analysis(dataDict,T,ClusterID, FullKey = FullKey)
             tmplookback, tmptemp, tmpcluster ,tmptracers,\
              tmphalo0 , tmpunbound, tmpotherHalo ,tmpnoHalo ,\
               tmpgas, tmpstars, tmpism , tmpwind ,tmpinflow, tmpoutflow \
-              = _inner_analysis(dataDict,T,ClusterID, FullKey = FullKey)
+              = _inner_analysis(snapdataDict,T,ClusterID=ClusterID, FullKey = Tkey, NtracersFull = NtracersFull)
             lookback.append(tmplookback[0])
             temp.append(tmptemp[0])
             cluster.append(tmpcluster[0])
@@ -172,14 +202,13 @@ def fullData_analyse_cluster(dataDict,paramStatsDict,Tlst,snapsRange,ClusterID=-
     StatsDF = pd.concat(dfList, axis=0, join='outer',sort=False, ignore_index=True)
     return StatsDF
 
-def clusteredData_analyse_cluster(dataDict,dtwparamStatsDict,Tlst,snapsRange,ignoreParams):
+def clusteredData_analyse_cluster(dataDict,dtwparamStatsDict,Tlst,snapsRange,FullDict,ignoreParams):
 
     dfList = []
     for T in Tlst:
         Tkey = f"T{T}"
         clusters =  np.unique(dataDict[Tkey]["clusters"])
         for clusterID in clusters:
-            whereCluster = np.where(dataDict[Tkey]["clusters"]==clusterID)[0]
 
             innerDF = pd.DataFrame({})
             lookback = []
@@ -197,6 +226,7 @@ def clusteredData_analyse_cluster(dataDict,dtwparamStatsDict,Tlst,snapsRange,ign
             inflow = []
             outflow = []
             for ii, snap in enumerate(snapsRange):
+                whereCluster = np.where(dataDict[Tkey]["clusters"][ii,:]==clusterID)[0]
                 tmpsnapdataDict = {}
                 for k, v in dataDict[Tkey].items():
                     if(np.shape(np.shape(v))[0]>1):
@@ -206,11 +236,13 @@ def clusteredData_analyse_cluster(dataDict,dtwparamStatsDict,Tlst,snapsRange,ign
                             tmpsnapdataDict.update({k : v[ii]})
                     else:
                         tmpsnapdataDict.update({k : v})
+                tmpsnapdataDict.update({'Ntracers' : [np.shape(tmpsnapdataDict['type'])[0]]})
+                NtracersFull = FullDict[Tkey]['Ntracers'][ii]
                 snapdataDict = {Tkey : tmpsnapdataDict}
                 tmplookback, tmptemp, tmpcluster ,tmptracers,\
                  tmphalo0 , tmpunbound, tmpotherHalo ,tmpnoHalo ,\
                   tmpgas, tmpstars, tmpism , tmpwind ,tmpinflow, tmpoutflow \
-                  = _inner_analysis(snapdataDict,T,ClusterID=clusterID, FullKey = Tkey)
+                  = _inner_analysis(snapdataDict,T,ClusterID=clusterID, FullKey = Tkey, NtracersFull = NtracersFull)
                 lookback.append(tmplookback[0])
                 temp.append(tmptemp[0])
                 cluster.append(tmpcluster[0])
@@ -241,8 +273,6 @@ def clusteredData_analyse_cluster(dataDict,dtwparamStatsDict,Tlst,snapsRange,ign
 
     StatsDF = pd.concat(dfList, axis=0, join='outer',sort=False, ignore_index=True)
     return StatsDF
-
-StatsDF = fullData_analyse_cluster(dataDict,paramStatsDict,Tlst,snapsRange,ClusterID=-1)
 
 clusteredDict = {}
 for T in Tlst:
@@ -304,12 +334,20 @@ for T in Tlst:
                 final.update({k : [v]})
     dtwparamStatsDict.update({key : final})
 
+for key, value in FlatDataDict.items():
+    tmp = []
+    for entry in value['type']:
+        tmp.append([np.shape(np.where(np.isnan(entry)==False)[0])[0]])
+    tmp = np.array(tmp)
+    FlatDataDict[key].update({'Ntracers' : tmp})
+    FlatDataDict[key].update({'Lookback' : clusteredDict[key]['Lookback']})
 
 savePath = DataSavepath + f"_DTW-Flat_Full-Data"+ DataSavepathSuffix
 print("\n" + f" Saving Merged Data as: "+ savePath)
 hdf5_save(savePath,clusteredDict)
 
-clusterStatsDF = clusteredData_analyse_cluster(clusteredDict,dtwparamStatsDict,Tlst,snapsRange,ignoreParams=ignoreParams)
+StatsDF = fullData_analyse_cluster(FlatDataDict,paramStatsDict,Tlst,snapsRange,ClusterID=-1)
+clusterStatsDF = clusteredData_analyse_cluster(clusteredDict,dtwparamStatsDict,Tlst,snapsRange,FullDict = FlatDataDict, ignoreParams=ignoreParams)
 
 finalStatsDF = pd.concat([StatsDF,clusterStatsDF], axis=0, join='outer',sort=False, ignore_index=True)
 
