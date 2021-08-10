DTW working with multi-halo subset. Added variable snapshot number for time averages on Bars. Fixing annotations on bars...

CGM_Multi-Phase/PlotMultiHaloTracers.py
@@ -128,9 +128,7 @@ selectTime = abs(
             )
         ]["Lookback"][0]
     )
-# ==============================================================================#
-#           PLOT!!
-# ==============================================================================#
+
 tlookback = []
 for snap in range(
         int(TRACERSPARAMS["snapMin"]),
@@ -146,6 +144,9 @@ for snap in range(
 
 tlookback = np.array(tlookback)
 
+#==============================================================================#
+#          Stats!
+#==============================================================================#
 
 statsData = multi_halo_stats(mergedDict,TRACERSPARAMS,saveParams,snapRange,Tlst)
 
@@ -203,5 +204,9 @@ print("Done!")
 #                   Bar Chart PLOT                                             #
 #==============================================================================#
 bars_plot(flatMergedDict,TRACERSPARAMS,saveParams,tlookback,selectTime,snapRange,Tlst,DataSavepath)
+matplotlib.rc_file_defaults()
+plt.close('all')
+
+bars_plot(flatMergedDict,TRACERSPARAMS,saveParams,tlookback,selectTime,snapRange,Tlst,DataSavepath,shortSnapRangeBool=True,shortSnapRangeNumber=4)
 matplotlib.rc_file_defaults()
 plt.close('all')

CGM_Multi-Phase/Tracers_Subroutines.py
@@ -3570,8 +3570,9 @@ def multi_halo_merge_flat_wrt_time(  simList,
                 for selectKey in dataDict.keys():
 
                     typeLen = np.shape(dataDict[selectKey]['type'])[1]
+                    subset = min(typeLen,dtwSubset)
 
-                    dtwSelect = random.sample([ ii for ii in range(0,typeLen,1)], k=dtwSubset)
+                    dtwSelect = random.sample([ ii for ii in range(0,typeLen,1)], k=subset)
 
                     for key in dataDict[selectKey].keys():
                         if key in loadParams: